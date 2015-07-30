1ff509679c8f2f11388c3cbfe08152d9a0291905
Andy@UDN <f255431@gmail.com>
2015-07-30 15:34:41 +0800

    first commit. Example completed.

---
+++ Singleton/Singleton.DaHuaExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.DaHuaExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance();
+            Singleton s2 = Singleton.GetInstance();
+
+            if (s1 == s2)
+            {
+                Console.WriteLine("The two objects are the same.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Singleton/Singleton.DaHuaExample/Singleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton.DaHuaExample
+{
+    class Singleton
+    {
+        private static Singleton instance;
+        private static readonly object syncRoot = new object();
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new Singleton();
+                    }
+                }
+            }
+
+            return instance;
+        }
+    }
+}
