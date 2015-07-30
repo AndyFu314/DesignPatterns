02157100addb4015754fd059da34a231b1658c4b
Andy@UDN <f255431@gmail.com>
2015-07-30 09:40:34 +0800

    first commit.

---
+++ Singleton/Singleton.Structural/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Singleton.Structural
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Constructor is protected, cannot use new
+            Singleton s1 = Singleton.Instance();
+            Singleton s2 = Singleton.Instance();
+
+            // Test for same instance
+            if (s1 == s2)
+            {
+                Console.WriteLine("Objects are the same instance.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Singleton
+    {
+        private static Singleton _instance;
+
+        // Constructor is 'protected'
+        protected Singleton()
+        {
+
+        }
+
+        public static Singleton Instance()
+        {
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
