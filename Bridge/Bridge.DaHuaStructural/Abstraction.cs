5a818b72d1affcb008c189c5ddd49f4b8172d5cd
Andy@UDN <f255431@gmail.com>
2015-07-31 13:54:09 +0800

    first commit. Bridge Structural

---
+++ Bridge/Bridge.DaHuaStructural/Abstraction.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.DaHuaStructural
+{
+    abstract class Abstraction
+    {
+        protected Implementor implementor;
+
+        public void setImplementor(Implementor implementor)
+        {
+            this.implementor = implementor;
+        }
+
+        public abstract void Operation();
+    }
+}
--- Bridge/Bridge.DaHuaStructural/Program.cs
+++ Bridge/Bridge.DaHuaStructural/Program.cs
@@ -10,6 +10,15 @@ namespace Bridge.DaHuaStructural
     {
         static void Main(string[] args)
         {
+            Abstraction ab = new RefinedAbstraction();
+
+            ab.setImplementor(new ConcreteImplementorA());
+            ab.Operation();
+
+            ab.setImplementor(new ConcreteImplementorB());
+            ab.Operation();
+
+            Console.ReadKey();
         }
     }
 }
