2d1de8c7ded924c458086804b6755fc61683dd34
Andy@UDN <f255431@gmail.com>
2015-07-31 15:33:24 +0800

    first commit. Composite Structural

---
+++ Composite/Composite.DaHuaStructural/Leaf.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composite.DaHuaStructural
+{
+    class Leaf: Component
+    {
+        public Leaf(string name) : base(name) { }
+        public override void Add(Component c)
+        {
+            Console.WriteLine("Can not add to a leaf.");
+        }
+
+        public override void Remove(Component c)
+        {
+            Console.WriteLine("Can not remove from a leaf.");
+        }
+
+        public override void Display(int depth)
+        {
+            Console.WriteLine(new String('-', depth) + name);
+        }
+    }
+}
