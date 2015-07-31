564e451bd1a6dace6baeb94aa0a8eb935f59b7e8
Andy@UDN <f255431@gmail.com>
2015-07-31 13:36:47 +0800

    first commit. Bridge CellPhone

---
+++ Bridge/Bridge.CellPhone/CellPhoneBrand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.CellPhone
+{
+    abstract class CellPhoneBrand
+    {
+        protected CellPhoneSoft soft;
+
+        public void SetCellPhoneSoft(CellPhoneSoft soft)
+        {
+            this.soft = soft;
+        }
+
+        public abstract void Run();
+    }
+}
---
+++ Bridge/Bridge.CellPhone/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bridge.CellPhone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CellPhoneBrand M4;
+            CellPhoneBrand Z3;
+
+            M4 = new CellPhoneBrandN();
+
+            M4.SetCellPhoneSoft(new CellPhoneGame());
+            M4.Run();
+            M4.SetCellPhoneSoft(new CellPhoneAddressList());
+            M4.Run();
+
+            Z3 = new CellPhoneBrandM();
+
+            Z3.SetCellPhoneSoft(new CellPhoneGame());
+            Z3.Run();
+            Z3.SetCellPhoneSoft(new CellPhoneAddressList());
+            Z3.Run();
+
+            Console.ReadKey();
+        }
+    }
+}
